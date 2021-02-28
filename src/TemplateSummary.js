4140e0b72887266900c7d33f892bb7311a2eb870
Shehzad Daredia <shehzad.daredia@gmail.com>
2021-02-28 11:26:43 -0600

    Add hover state to template summary tiles

--- src/TemplateSummary.js
+++ src/TemplateSummary.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from "classnames";
 
 const TemplateSummary = ({ name, body, onClick }) => {
-  if (!name || !body)
-    return <></>;
+  const [isHovered, setHoverState] = useState(false);
 
+  const toggleHover = () => setHoverState(!isHovered);
   const handleClick = e => {
     // Don't open a link that may have been clicked inside the template - the user can
     // subsequently click on links within the detail view instead
@@ -11,9 +12,17 @@ const TemplateSummary = ({ name, body, onClick }) => {
     onClick();
   }
 
+  const titleCls = classNames({
+    'ts-title': true,
+    hovered: isHovered,
+  });
+
+  if (!name || !body)
+    return <></>;
+
   return (
-    <div className="template-summary" onClick={handleClick}>
-      <div className="ts-title">{name}</div>
+    <div className="template-summary" onClick={handleClick} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+      <div className={titleCls}>{name}</div>
       {/* Template body contains raw, unescaped html that has been sanitized on the server */}
       <div className="ts-body" dangerouslySetInnerHTML={{ __html: body }} />
     </div>
