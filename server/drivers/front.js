1df9db03b5d77a6657045fad2f4fc82175f4e961
Shehzad Daredia <shehzad.daredia@gmail.com>
2021-03-21 11:51:02 -0400

    Check for pagination on server response and aggregate results accordingly

--- server/drivers/front.js
+++ server/drivers/front.js
@@ -2,32 +2,44 @@ const fetch = require('node-fetch');
 
 const apiHost = 'https://api2.frontapp.com';
 
-const getTeamMessageTemplates = async () => {
-  console.log({msg: 'Fetching team message templates'});
-
-  const endpoint = `/teams/${process.env.FRONT_MASTER_TEAM_ID}/responses`;
-
-  const response = await fetch(`${apiHost}${endpoint}`, {
+const fetchAndValidate = async (url) => {
+  const response = await fetch(url, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       Authorization: `Bearer ${process.env.FRONT_API_TOKEN}`,
     },
-  })
-  .then(r => {
-    if (!r.ok && r.status !== 404)
-      throw Error(r.statusText);
+  });
 
-    if(r.status === 404)
-      return ({});
+  if (!response.ok && response.status !== 404)
+      throw Error(response.statusText);
 
-    return r.json();
-  })
-  // TODO(shez): check for pagination and aggregate results accordingly
-  .then(response => response._results || [])
-  .catch(err => { throw err; });
+  if (response.status === 404)
+    return ({});
 
-  return response;
+  const json = await response.json();
+  return json;
+};
+
+const getTeamMessageTemplates = async () => {
+  console.log({msg: 'Fetching team message templates'});
+
+  const endpoint = `/teams/${process.env.FRONT_MASTER_TEAM_ID}/responses`;
+  const response = await fetchAndValidate(`${apiHost}${endpoint}`);
+  const {_results, _pagination, _error} = response;
+
+  let nextPageUrl = _pagination?.next;
+  const results = _results;
+
+  while (nextPageUrl) {
+    const response = await fetchAndValidate(nextPageUrl);
+    const {_results, _pagination, _error} = response;
+
+    nextPageUrl = _pagination?.next;
+    results.push(..._results);
+  }
+
+  return results || [];
 };
 
 module.exports = {
